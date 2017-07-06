f13a6e81cb0c4084e49f6abf189d9a633073b322
MyatThuHeinn <myatthu.136@gmail.com>
2017-07-06 21:23:40 +0630

    Firebase complete

--- mth-social3/FeedVC.swift
+++ mth-social3/FeedVC.swift
@@ -13,6 +13,8 @@ import Firebase
 class FeedVC: UIViewController,UITableViewDelegate,UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
+    
+    var posts = [Post]()
 
     @IBAction func signOutTapped(_ sender: Any) {
         
@@ -34,9 +36,23 @@ class FeedVC: UIViewController,UITableViewDelegate,UITableViewDataSource {
             tableView.dataSource = self
         
         DataService.ds.REF_POSTS.observe(.value, with: {(snapshot) in
-            print("GG")
-            print(snapshot.value)
+            
+            self.posts = []
+            
+            if let snapshot = snapshot.children.allObjects as? [FIRDataSnapshot]{
+                for snap in snapshot{
+                    print("SNAP: \(snap)")
+                    if let postDict = snap.value as? Dictionary<String,AnyObject>{
+                        let key = snap.key
+                        let post = Post(postKey: key, postData: postDict)
+                        self.posts.append(post)
+                    }
+                }
+            }
+            self.tableView.reloadData()
         })
+        
+        
        
     }
     
@@ -45,10 +61,14 @@ class FeedVC: UIViewController,UITableViewDelegate,UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        
+        return self.posts.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let post = posts[indexPath.row]
+        print("JESS: \(post.caption)")
         return tableView.dequeueReusableCell(withIdentifier: "PostCell")!
     }
 
---
+++ mth-social3/Post.swift
@@ -0,0 +1,57 @@
+//
+//  Post.swift
+//  mth-social3
+//
+//  Created by Myat Thu Hein on 7/5/17.
+//  Copyright © 2017 Myat Thu Hein. All rights reserved.
+//
+
+import Foundation
+
+class Post{
+
+    private var _caption: String!
+    private var _imgUrl: String!
+    private var _likes: Int!
+    private var _postKey: String!
+    
+    var caption:String{
+        return _caption
+    }
+    
+    var imgUrl:String{
+        return _imgUrl
+    }
+    
+    var likes:Int{
+        return _likes
+    }
+    
+    var postKey:String{
+        return _postKey
+    }
+    
+    
+    init(caption:String,imgUrl:String,likes:Int){
+        self._caption = caption
+        self._imgUrl = imgUrl
+        self._likes = likes
+    }
+    
+    init(postKey: String,postData: Dictionary<String,AnyObject>){
+        self._postKey = postKey
+        if let caption = postData["caption"]{
+            self._caption = caption as! String
+        }
+        
+        if let imgUrl = postData["imgUrl"]{
+            self._imgUrl = imgUrl as! String
+        }
+        
+        
+        if let likes = postData["likes"] as? Int{
+            self._likes = likes
+        }
+    }
+    
+}
